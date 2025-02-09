9138b86966f08036f873725b23a526b38c3f5aab
kooflixy <kooflixy@gmail.com>
2025-02-09 15:41:42 +0300

    Add logging

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello world')
+import logging
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filename=".log",filemode="a", encoding='utf-8',
+                        format="%(asctime)s %(levelname)s %(message)s")
+    
+    print('start')
