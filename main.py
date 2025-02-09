02d61d521975ef14f4abd214184c8f14c04e1ff0
kooflixy <kooflixy@gmail.com>
2025-02-09 21:47:35 +0300

    a little changes

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import logging
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, filename=".log",filemode="a", encoding='utf-8',
-                        format="%(asctime)s %(levelname)s %(message)s")
+                        format="%(asctime)s %(levelname)s:%(name)s %(message)s")
     
     print('start')
