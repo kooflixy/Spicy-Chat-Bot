50d9af4decd2c43b4481ddda23666455d41949d2
kooflixy <kooflixy@gmail.com>
2025-02-23 16:21:58 +0300

    add pydantic schema for user

---
+++ db/schemas.py
@@ -0,0 +1,13 @@
+import datetime
+from pydantic import BaseModel
+
+
+class UsersAddDTO(BaseModel):
+    id: int
+    username: str
+    char_id: str
+    conv_id: str
+
+class UsersDTO(UsersAddDTO):
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
