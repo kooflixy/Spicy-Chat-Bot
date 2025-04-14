a7a0bd9d0f74d5ca2908c1eb425b89d54eae6f76
kooflixy <kooflixy@gmail.com>
2025-04-14 12:51:51 +0300

    add getlogs func

--- tg_bot/handlers/admin.py
+++ tg_bot/handlers/admin.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from logging import getLogger
 from aiogram import F, Router
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 import config
 from tg_bot.contrib.active_chat_sesses_checker import active_chats_sesses_checker
 from tg_bot.contrib.func_logger import UserForLogs
@@ -18,3 +18,9 @@ async def getstats(message: Message):
 Активных пользователей: {active_chats_sesses_checker.count()}
 ''')
     logger.info(f'{getstats.__name__} is handled {UserForLogs.log_name(message)}')
+
+
+@router.message(F.text.casefold().in_(['/getlogs', '!логи']), IsAdmin(config.TG_ADMINS))
+async def getlogs(message: Message):
+    await message.answer_document(FSInputFile('.log', 'logs.log'))
+    logger.info(f'{getlogs.__name__} is handled {UserForLogs.log_name(message)}')
