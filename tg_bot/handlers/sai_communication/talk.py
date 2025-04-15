3a3b85131deac6604df816b9b4356da5f15bed73
kooflixy <kooflixy@gmail.com>
2025-04-15 22:40:18 +0300

    add try in talk func

--- tg_bot/handlers/sai_communication/talk.py
+++ tg_bot/handlers/sai_communication/talk.py
@@ -19,19 +19,23 @@ async def talk_with_sai_bot(message: Message):
     '''Sends users's message to SpicyChat and gets response message'''
     if active_chats_sesses_checker.check(message.chat.id): return
 
-    async with async_session_factory() as session:
-        active_chats_sesses_checker.add(message.chat.id)
+    try:
+        async with async_session_factory() as session:
+            active_chats_sesses_checker.add(message.chat.id)
 
-        user = await session.get(UsersORM, message.chat.id)
+            user = await session.get(UsersORM, message.chat.id)
 
-        if message.chat.type == 'private':
-            bot_message = await spicy_api.send_message(message=message.text, char_id=user.char_id, conv_id=user.conv_id, username=message.from_user.full_name)
+            if message.chat.type == 'private':
+                bot_message = await spicy_api.send_message(message=message.text, char_id=user.char_id, conv_id=user.conv_id, username=message.from_user.full_name)
+                
+                await message.answer(bot_message)
+            else:
+                bot_message = await spicy_api.send_message(message=message.text, char_id=user.char_id, conv_id=user.conv_id, username=message.from_user.full_name)
+
+                await message.reply(bot_message)
             
-            await message.answer(bot_message)
-        else:
-            bot_message = await spicy_api.send_message(message=message.text, char_id=user.char_id, conv_id=user.conv_id, username=message.from_user.full_name)
-
-            await message.reply(bot_message)
-        
+            active_chats_sesses_checker.remove(message.chat.id)
+            logger.info(f'{talk_with_sai_bot.__name__} is handled {UserForLogs.log_name(message)}')
+    except Exception as ex:
         active_chats_sesses_checker.remove(message.chat.id)
-        logger.info(f'{talk_with_sai_bot.__name__} is handled {UserForLogs.log_name(message)}')
+        logger.info(f'{talk_with_sai_bot.__name__} is handled {UserForLogs.log_name(message)}: error {ex}')
